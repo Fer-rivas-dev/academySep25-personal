1fe38be2d2b14807cc28d2b1e1f21c09469fbce8
Miguel Rugerio <miguel.rugerio@outlook.com>
2025-09-09 14:06:49 -0600

    Inicio proyecto MVC & Scripts mysql

--- inicio/src/com/curso/v0/Principal.java
+++ inicio/src/com/curso/v0/Principal.java
@@ -1,10 +1,16 @@
 package com.curso.v0;
 
+import java.io.PrintStream;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
+		System.err.println("Hello World");
+		
+		PrintStream ps = System.out;
+		ps.println("Hola Mundo");
+		
 	}
 
 }
