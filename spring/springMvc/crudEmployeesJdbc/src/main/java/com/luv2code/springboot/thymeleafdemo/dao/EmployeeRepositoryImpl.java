a9173351e1f69e3e810f8726bbacdceafec8ede8
Miguel Rugerio <miguel.rugerio@outlook.com>
2025-09-09 17:41:50 -0600

    Implementar todos los metodos

--- spring/springMvc/crudEmployeesJdbc/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepositoryImpl.java
+++ spring/springMvc/crudEmployeesJdbc/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepositoryImpl.java
@@ -53,17 +53,70 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 
     @Override
     public Optional<Employee> findById(int id) {
-        // TODO Auto-generated method stub
+        String sql = "SELECT * FROM employee WHERE id = ?";
+        
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+                int empId = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String email = resultSet.getString("email");
+                Employee employee = new Employee(empId, firstName, lastName, email);
+                return Optional.of(employee);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
         return Optional.empty();
     }
 
     @Override
     public void save(Employee employee) {
-        // TODO Auto-generated method stub
+        String sql;
+        
+        if (employee.getId() == 0) {
+            sql = "INSERT INTO employee (first_name, last_name, email) VALUES (?, ?, ?)";
+        } else {
+            sql = "UPDATE employee SET first_name = ?, last_name = ?, email = ? WHERE id = ?";
+        }
+        
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            
+            statement.setString(1, employee.getFirstName());
+            statement.setString(2, employee.getLastName());
+            statement.setString(3, employee.getEmail());
+            
+            if (employee.getId() != 0) {
+                statement.setInt(4, employee.getId());
+            }
+            
+            statement.executeUpdate();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteById(int id) {
-        // TODO Auto-generated method stub
+        String sql = "DELETE FROM employee WHERE id = ?";
+        
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
