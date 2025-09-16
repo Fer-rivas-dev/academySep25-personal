9ca95224b39b02896efd8101b0c0431d838b29e5
Miguel Rugerio <miguel.rugerio@outlook.com>
2025-09-16 16:54:39 -0600

    Implementar con Mockito la busqueda de un cliente en el Service

--- spring/springRest/crudMongoDb/src/main/java/com/mongodb/crud/model/Cliente.java
+++ spring/springRest/crudMongoDb/src/main/java/com/mongodb/crud/model/Cliente.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Document(collection = "clientes")
 public class Cliente {
@@ -71,4 +72,18 @@ public class Cliente {
     public void setFechaRegistro(LocalDateTime fechaRegistro) {
         this.fechaRegistro = fechaRegistro;
     }
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+    
+    
 }
--- spring/springRest/crudMongoDb/src/test/java/com/mongodb/crud/ClienteServiceTests.java
+++ spring/springRest/crudMongoDb/src/test/java/com/mongodb/crud/ClienteServiceTests.java
@@ -1,7 +1,11 @@
 package com.mongodb.crud;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import com.mongodb.crud.model.Cliente;
 import com.mongodb.crud.repository.ClienteRepository;
-import com.mongodb.crud.service.ClienteServiceImpl;
+import com.mongodb.crud.service.ClienteService;
 
 @SpringBootTest
 public class ClienteServiceTests {
@@ -19,21 +24,27 @@ public class ClienteServiceTests {
     private ClienteRepository clienteRespository;
 	
 	@Autowired
-	private ClienteServiceImpl clienteServiceImpl;
+	private ClienteService clienteService;
 
 	@Test
 	@DisplayName("Consulta de un cliente")
 	void buscarClienteId() {
-		when(clienteRespository.findById("5").
-				//REGRESAR EL CLIENTE 5
-				thenReturn( );
+		Cliente cliente = new Cliente("Filologo Pérez", "filologo@email.com", "123456789");
+		cliente.setId("5");
 		
+		Cliente clienteEsperado = new Cliente("Filologo Pérez", "filologo@email.com", "123456789");
+		clienteEsperado.setId("5");
+
+		when(clienteRespository.findById("5"))
+				//REGRESAR EL CLIENTE 5
+				.thenReturn(Optional.of(clienteEsperado));
+
 		assertEquals(
 				//COMPARAR LOS DOS CLIENTES CON ID 5 Y DEBEN SER IGUALES
-				clienteServiceImpl.findById("5"),
-				
-				);
+				Optional.of(cliente),
+				clienteService.findById("5"));
 		
+		verify(clienteRespository, times(1)).findById("5");
 	}
 	
 }
