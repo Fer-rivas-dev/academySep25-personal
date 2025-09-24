15f6df8cb0796278d8dcaab7f1bd2fc66a6475a0
Miguel Rugerio <miguel.rugerio@outlook.com>
2025-09-24 14:06:18 -0600

    Proyecto SpringBoot DDD, primer flujo de entrada

---
+++ spring/smartparking-system/src/main/java/com/javatechie/smartparking_system/SmartparkingSystemApplication.java
@@ -0,0 +1,29 @@
+package com.javatechie.smartparking_system;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.javatechie.smartparking_system.allocation.Slot;
+import com.javatechie.smartparking_system.allocation.SlotRepository;
+
+@SpringBootApplication
+public class SmartparkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartparkingSystemApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner initSlots(SlotRepository repo) {
+		return args -> {
+			if (repo.count() == 0) {
+				repo.save(new Slot(null, "A1", true, null));
+				repo.save(new Slot(null, "A2", true, null));
+				repo.save(new Slot(null, "B1", true, null));
+			}
+		};
+	}
+
+}
